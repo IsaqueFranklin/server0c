5d5c5b29fe597084ea630a7e0abdf8c726a81933
Isaque Franklin <isaquefrankli@gmail.com>
2024-11-12 20:00:59 -0300

    easy-http bugs fixed.

--- easy-http/server.h
+++ easy-http/server.h
@@ -1,4 +1,4 @@
-#ifdef server_h
+#ifndef server_h
 #define server_h
 
 #include <netinet/in.h>
@@ -6,17 +6,17 @@
 #define BUFFER_SIZE 16000
 
 struct Server {
-  int domain;
-  int port;
-  int service;
-  int protocol;
-  int backlog;
-  u_long interface;
+    int domain;
+    int port;
+    int service;
+    int protocol;
+    int backlog;
+    u_long interface;
 
-  int socket;
-  struct sockaddr_in address;
+    int socket;
+    struct sockaddr_in address;
 
-  void (*launch)(struct Server *server);
+    void (*launch)(struct Server *server);
 };
 
 struct Server server_Constructor(int domain, int port, int service, int protocol, int backlog, u_long interface, void (*launch)(struct Server *server));
