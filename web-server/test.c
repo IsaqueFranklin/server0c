7afb8f7556083b73a7e85dd2718fc1d811b19849
Isaque Franklin <isaquefrankli@gmail.com>
2024-11-13 17:39:55 -0300

    More bugs fixed on web-server dir

--- web-server/test.c
+++ web-server/test.c
@@ -10,18 +10,21 @@
 void launch(struct Server *server) {
 
   char buffer[30000];
-
-  printf("==== WAITING FOR CONNECTION ==== \n");
-
+  char *hello = "HTTP/1.1 200 OK\nDate: Wed, 13 Nov 2024 12:28:63 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Web, 13 Nov 2024 19:15:56 GMT\nContent-Type: text/html\nConnection: Closed\n\n<html><body><h1>Hello, world!</h1></body></html>";
   int address_length = sizeof(server->address);
-  int new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&address_length);
-  read(new_socket, buffer, 30000);
+  int new_socket;
 
-  printf("%s\n", buffer);
-  char *hello = "HTTP/1.1 200 OK\nDate: Wed, 13 Nov 2024 12:28:63 GMT\nServer: Apache/2.2.14 (Win32)\nLast-Modified: Web, 13 Nov 2024 19:15:56 GMT\nContent_Length: 88\nContent-Type: text/html\nConnection: Closed\n<html><body>Mh1>Stuff</h1>M</body></html>";
-  write(new_socket, hello, strlen(hello));
+  while (1) {
+    printf("==== WAITING FOR CONNECTION ==== \n");
 
-  close(new_socket);
+    new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&address_length);
+    read(new_socket, buffer, 30000);
+
+    printf("%s\n", buffer);
+    write(new_socket, hello, strlen(hello));
+
+    close(new_socket);
+  }  
 }
 
 int main() {
